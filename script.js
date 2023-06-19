d4a92676b3664f45f92e4b405bb1526657403137
Ashir.Jr <91126593+Ashir-jr@users.noreply.github.com>
2023-06-19 17:14:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-$(document).ready(function () {
-    var count = 0;
-  
-    $(".tarot-card").click(function () {
-      var card = $(this);
-      if (count < 11) {
-        $(this).removeClass("top").addClass("bottom");
-  
-        count++;
-      }
-      if (count == 11) {
-        $("div").removeClass("bottom");
-  
-        count = 0;
-      }
-      shuffle(card);
-    });
+// Function to check if the user is on a mobile device
+function isMobileDevice() {
+  return (
+    typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf("IEMobile") !== -1
+  );
+}
+
+// Function to add mobile-specific behavior
+function setupMobileBehavior() {
+  // Hide the shuffle button
+  $(".tarot-card").off("click"); // Remove click event on cards
+}
+
+// Function to add desktop-specific behavior
+function setupDesktopBehavior() {
+  var count = 0;
+
+  $(".tarot-card").click(function () {
+    var card = $(this);
+    if (count < 11) {
+      $(this).removeClass("top").addClass("bottom");
+
+      count++;
+    }
+    if (count == 11) {
+      $("div").removeClass("bottom");
+
+      count = 0;
+    }
+    shuffle(card);
   });
-  
-  function shuffle(card) {
-    TweenLite.fromTo(
-      card,
-      0.6,
-      {
-        x: 410,
-        y: -15,
-        ease: Expo.easeOut
-      },
-      {
-        x: 0,
-        y: 0,
-        ease: Expo.easeIn
-      }
-    );
-  }
+}
+
+$(document).ready(function () {
+  if (isMobileDevice()) {
+    setupMobileBehavior();
+  } else {
+    setupDesktopBehavior();
+  }
+});
