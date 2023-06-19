61fadc5c1d6ee98c4896659026324bd1958d7e30
Ashir.Jr <91126593+Ashir-jr@users.noreply.github.com>
2023-06-19 18:16:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-// Function to check if the user is on a mobile device
-function isMobileDevice() {
-  return (
-    typeof window.orientation !== "undefined" ||
-    navigator.userAgent.indexOf("IEMobile") !== -1
-  );
-}
-
-// Function to add mobile-specific behavior
-function setupMobileBehavior() {
-  // Hide the shuffle button
-  $(".tarot-card").off("click"); // Remove click event on cards
-}
-
-// Function to add desktop-specific behavior
-function setupDesktopBehavior() {
-  var count = 0;
-
-  $(".tarot-card").click(function () {
-    var card = $(this);
-    if (count < 11) {
-      $(this).removeClass("top").addClass("bottom");
-
-      count++;
-    }
-    if (count == 11) {
-      $("div").removeClass("bottom");
-
-      count = 0;
-    }
-    shuffle(card);
-  });
-}
-
 $(document).ready(function () {
   if (isMobileDevice()) {
-    setupMobileBehavior();
-  } else {
-    setupDesktopBehavior();
+    var count = 0;
+
+    $(".tarot-card").click(function () {
+      var card = $(this);
+      if (count < 11) {
+        $(this).removeClass("top").addClass("bottom");
+
+        count++;
+      }
+      if (count == 11) {
+        $("div").removeClass("bottom");
+
+        count = 0;
+      }
+      shuffle(card);
+    });
   }
 });
+
+function shuffle(card) {
+  TweenLite.fromTo(
+    card,
+    0.6,
+    {
+      x: 410,
+      y: -15,
+      ease: Expo.easeOut
+    },
+    {
+      x: 0,
+      y: 0,
+      ease: Expo.easeIn
+    }
+  );
+}
